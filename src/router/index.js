8a66042bf7441e65c5117a1eeb23c3194bce79d5
戚炜淳 <1079713055@qq.com>
2021-09-07 15:11:01 +0800

    some settings
    setting.json & vue.config.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = []
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 export default router
